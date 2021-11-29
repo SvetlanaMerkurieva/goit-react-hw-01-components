3b6314db286aec595b5db3d3d234dbe5b9828d55
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-11-29 23:21:49 +0300

    Добавляет стили в Список друзей

--- src/friends/FriendList.js
+++ src/friends/FriendList.js
@@ -1,5 +1,5 @@
 import friends from './friends.json';
-import styles from './FriendList.modules.css';
+import styles from './FriendList.module.css';
 
 export const FriendList = () => {
     return (
@@ -8,16 +8,17 @@ export const FriendList = () => {
         </ul>
     )
 }
-
 const FriendListItem = () => {
     return (
         <>
             {friends.map(friend => (
-                <li className={styles.Item}>
-                    <span className={styles.Status}>{friend.isOnline}</span>
-                    <img className={styles.Avatar} src={friend.avatar} alt="User avatar" width="48" />
-                    <p className={styles.Name}>{friend.name}</p>
-                </li>
+                <div className={styles.Card}>
+                    <li className={styles.Item}>
+                        <span className={styles.Status}></span>
+                        <img className={styles.Avatar} src={friend.avatar} alt="User avatar" width="48" />
+                        <p className={styles.Name}>{friend.name}</p>
+                    </li>
+                </div>
             ))}
         </>
     )
