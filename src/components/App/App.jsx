465dd0667b6b2eed03fb671a35996a8c5acacdb0
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-12-03 12:34:10 +0200

    Edits styles App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,29 +6,29 @@ import { Profile } from '../Profile/Profile';
 import { Statistics } from '../Statistics/Statistics';
 import { FriendList } from '../Friends/FriendList';
 import { Transactions } from '../Transactions/Transactions';
-import './App.module.css';
+import s from './App.module.css';
 
 export default function App() {
-   return (
-      <div className="app">
-         <header className="app-header">
-            <h1>React Homework № 1</h1>
-            <h2 className="title">Профиль социальной сети</h2>
-            <Profile
-               username={user.username}
-               tag={user.tag}
-               location={user.location}
-               avatar={user.avatar}
-               stats={user.stats}
-            />
-            <h2 className="title">Секция статистики</h2>
-            <Statistics title="Unload stats" stats={data} />
-            <Statistics stats={data} />
-            <h2 className="title">Список друзей</h2>
-            <FriendList friends={friends} />
-            <h2 className="title">История транзакций</h2>
-            <Transactions items={transactions} />
-         </header>
-      </div>
-   );
+  return (
+    <div className={s.app}>
+      <header className={s.appHeader}>
+        <h1>React Homework № 1</h1>
+        <h2 className={s.title}>Профиль социальной сети</h2>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <h2 className={s.title}>Секция статистики</h2>
+        <Statistics title="Unload stats" stats={data} />
+        <Statistics stats={data} />
+        <h2 className={s.title}>Список друзей</h2>
+        <FriendList friends={friends} />
+        <h2 className={s.title}>История транзакций</h2>
+        <Transactions items={transactions} />
+      </header>
+    </div>
+  );
 }
